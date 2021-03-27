0b6c09eb51a1f21b1c5671870f1133a712f7bade
PyGuy <75491816+TechnoShip123@users.noreply.github.com>
2021-03-27 16:16:14 -0500

    Testing antispam, right now it detects mentions

---
+++ cogs/tools/antispam.py
@@ -0,0 +1,23 @@
+from discord.ext import commands
+from assets.functions import *
+
+
+class Antispam(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @Cog.listener()
+	async def on_message(self, message):
+		def _check(m):
+			return (m.author == message.author
+					and len(m.mentions)
+					and (datetime.utcnow()-m.created_at).seconds < 60)
+
+		if not message.author.bot:
+			if len(list(filter(lambda m: _check(m), self.bot.cached_messages))) >= 3:  # If we detect spamming
+				await message.channel.send("Don't spam mentions!", delete_after=10)
+                await message.channel.send("https://i.imgur.com/LzDKgZN.png")
+
+
+def setup(bot):
+    bot.add_cog(Antispam(bot))
