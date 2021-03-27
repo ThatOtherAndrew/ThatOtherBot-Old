b96822449c4f36d06899f0c3c824b3c9e587cda5
PyGuy <75491816+TechnoShip123@users.noreply.github.com>
2021-03-27 16:26:55 -0500

    Update antispam.py

--- cogs/tools/antispam.py
+++ cogs/tools/antispam.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from discord.ext.commands import Cog
 from assets.functions import *
 
 
@@ -7,15 +8,15 @@ class Antispam(commands.Cog):
         self.bot = bot
 
     @Cog.listener()
-	async def on_message(self, message):
-		def _check(m):
-			return (m.author == message.author
-					and len(m.mentions)
-					and (datetime.utcnow()-m.created_at).seconds < 60)
+    async def on_message(self, message):
+        def _check(m):
+            return (m.author == message.author
+                    and len(m.mentions)
+                    and (datetime.utcnow() - m.created_at).seconds < 60)
 
-		if not message.author.bot:
-			if len(list(filter(lambda m: _check(m), self.bot.cached_messages))) >= 3:  # If we detect spamming
-				await message.channel.send("Don't spam mentions!", delete_after=10)
+        if not message.author.bot:
+            if len(list(filter(lambda m: _check(m), self.bot.cached_messages))) >= 3:  # If we detect spamming
+                await message.channel.send("Don't spam mentions!", delete_after=10)
                 await message.channel.send("https://i.imgur.com/LzDKgZN.png")
 
 
