ca8b301feb1943b4f32f1a7828ad89c6c9071fc8
Andrew <stroev.andrew@gmail.com>
2021-05-12 12:47:06 +0100

    🐛 Fixed query URL for follow-up queries

--- cogs/web/wolframalpha.py
+++ cogs/web/wolframalpha.py
@@ -1,7 +1,7 @@
 import aiohttp
 from discord.ext import commands
 from assets.functions import env, initembed
-from munch import munchify
+from munch import Munch, munchify
 from urllib.parse import quote_plus
 
 
@@ -9,7 +9,7 @@ class WolframAlpha(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.conversations = {}
+        self.conversations = Munch()
 
     @commands.command(aliases=["wa", "wolfram", "alpha", "grumbot"])
     async def wolframalpha(self, ctx, *, args=None):
@@ -18,16 +18,24 @@ class WolframAlpha(commands.Cog):
             return
 
         async with ctx.channel.typing():
-            url = "https://api.wolframalpha.com/v1/conversation.jsp?" \
+            if ctx.author.id not in self.conversations:
+                self.conversations[ctx.author.id] = munchify({"host": "", "id": "", "s": ""})
+
+            url = f"https://{self.conversations[ctx.author.id].host or 'api.wolframalpha.com'}/v1/conversation.jsp?" \
                 f"appid={env.WOLFRAMTOKEN}" \
                 f"&i={quote_plus(args)}"
-            url += f"&conversationid={self.conversations[ctx.author.id]}" if ctx.author.id in self.conversations else ""
+            if self.conversations[ctx.author.id].id:
+                url += f"&conversationid={self.conversations[ctx.author.id].id}"
+            if self.conversations[ctx.author.id].s:
+                url += f"&s={self.conversations[ctx.author.id].s}"
 
             async with aiohttp.request("GET", url) as resp:
-                response = munchify(await resp.json())
+                response = munchify(await resp.json(content_type="application/json"))
 
             if not hasattr(response, "error"):
-                self.conversations[ctx.author.id] = response.conversationID
+                self.conversations[ctx.author.id].host = response.host + "/api"
+                self.conversations[ctx.author.id].id = response.conversationID
+                self.conversations[ctx.author.id].s = response.s if hasattr(response, "s") else ""
                 e = initembed(ctx, "Wolfram|Alpha", bordercolour=0xFF6600)
                 e.add_field(name="Response", value=response.result)
             else:
